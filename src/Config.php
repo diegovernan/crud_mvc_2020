8820676973d3639e4188ed57a9b3db432f227cf8
Diego Vernan <diegovernan@gmail.com>
2020-04-20 19:13:21 -0300

    primeiro commit

--- src/Config.php
+++ src/Config.php
@@ -2,11 +2,11 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/mvc-2020/public';
+    const BASE_DIR = '/crud-mvc-2020/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'mvc-2020';
+    const DB_DATABASE = 'crud-mvc-2020';
     const DB_USER = 'root';
     const DB_PASS = 'root';
 
